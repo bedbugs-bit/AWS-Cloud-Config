b72130c3615a736892aaab7b05a7360e55994848
Joel Obowu <57295185+bedbugs-bit@users.noreply.github.com>
2025-04-04 11:31:40 +0000

    add AWS SDK for Go lang and Java

---
+++ sdk/go/go.mod
@@ -0,0 +1,5 @@
+module s3-demo
+
+go 1.21
+
+require github.com/aws/aws-sdk-go-v2 v1.24.1
---
+++ sdk/go/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+    "context"
+    "fmt"
+    "log"
+
+    "github.com/aws/aws-sdk-go-v2/aws"
+    "github.com/aws/aws-sdk-go-v2/config"
+    "github.com/aws/aws-sdk-go-v2/service/s3"
+)
+
+func main() {
+    // Load AWS config from environment variables
+    cfg, err := config.LoadDefaultConfig(context.TODO())
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    // Create S3 client
+    client := s3.NewFromConfig(cfg)
+
+    // List buckets
+    result, err := client.ListBuckets(context.TODO(), &s3.ListBucketsInput{})
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    fmt.Println("S3 Buckets:")
+    for _, bucket := range result.Buckets {
+        fmt.Println("-", *bucket.Name)
+    }
+}
